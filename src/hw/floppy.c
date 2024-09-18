d75920d6d8a29f4fd8850b641a4147513e83c983
Petr Cvek <petrcvekcz@gmail.com>
2024-09-19 01:29:06 +0200

    Allow booting from floppy outside QEMU
    but disable it until boot ordering (so it doesn't lag boot)

--- src/hw/floppy.c
+++ src/hw/floppy.c
@@ -155,7 +155,7 @@ floppy_setup(void)
         return;
     dprintf(3, "init floppy drives\n");
 
-    if (CONFIG_QEMU) {
+    if (CONFIG_QEMU || 0) {        //TODO floppy disabled for now
         u8 type = rtc_read(CMOS_FLOPPY_DRIVE_TYPE);
         if (type & 0xf0)
             addFloppy(0, type >> 4);
